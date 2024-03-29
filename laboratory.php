a59eca7462225c32c4a2ab6b74caf75aeb2435a3
ParvinderSvt <161319060+ParvinderSvt@users.noreply.github.com>
2024-03-29 09:35:06 +0530

    short update done

--- laboratory.php
+++ laboratory.php
@@ -31,42 +31,6 @@
         <!-- breadcrumb-area-end -->
 
 
- <div class="homepage-slides owl-carousel">
- 		<div class="single-slide-item" style="background-image: url(img/banner1-min.jpg);">
- 			
- 			<div class="hero-area-content">
- 				<div class="container">
- 					<div class="row justify-content-center">
- 						<div class="col-lg-12 wow fadeInUp animated" data-wow-delay=".3s">
- 							 <div class="section-title">
- 								<!-- <h1>Welcome To<br> Radha Ballabh Public School</h1>
- 								<p> Curriculum based on International Standards, imbibed with meaningful and appropriate activities to enable holistic development of children</p> -->
- 							</div>
- 						 
- 						</div>
- 					</div>
- 				</div>
- 			</div>
- 		</div>
-
- 		<div class="single-slide-item hero-area-bg-2"  style="background-image: url(img/banner2-min.jpg);">
- 		
- 			<div class="hero-area-content">
- 				<div class="container">
- 					<div class="row justify-content-center">
- 						<div class="col-lg-12 wow fadeInUp animated" data-wow-delay=".3s">
- 							<div class="section-title">
- 								 <!-- <h1>Educaton is the Key<br> to Success</h1>
- 									<p> Curriculum based on International Standards, imbibed with meaningful and appropriate activities to enable holistic development of children</p> -->
- 							</div>
- 							 
- 						</div>
- 					</div>
- 				</div>
- 			</div>
- 		</div>
- 	</div>  
+ 
 
 
-
-<?php include 'footer.php'; ?>
